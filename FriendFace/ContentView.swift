9b95a3dd219d0bd4424da550632a6a2c21d3f817
Milo Wyner <milowyner@gmail.com>
2021-08-29 12:58:14 -0700

    Move network code into its own function

--- FriendFace/ContentView.swift
+++ FriendFace/ContentView.swift
@@ -16,20 +16,22 @@ struct ContentView: View {
             Text("\(user.age)")
             Text(user.company)
         }
-        .onAppear(perform: {
-            let url = URL(string: "https://www.hackingwithswift.com/samples/friendface.json")!
-            URLSession.shared.dataTask(with: url) { data, response, error in
-                guard let data = data else {
-                    print("Error fetching data. \(error?.localizedDescription ?? "Unkown error.")")
-                    return
-                }
-                do {
-                    users = try JSONDecoder().decode([User].self, from: data)
-                } catch {
-                    print("Error decoding data. \(error.localizedDescription)")
-                }
-            }.resume()
-        })
+        .onAppear(perform: loadUsers)
+    }
+    
+    func loadUsers() {
+        let url = URL(string: "https://www.hackingwithswift.com/samples/friendface.json")!
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data else {
+                print("Error fetching data. \(error?.localizedDescription ?? "Unkown error.")")
+                return
+            }
+            do {
+                users = try JSONDecoder().decode([User].self, from: data)
+            } catch {
+                print("Error decoding data. \(error.localizedDescription)")
+            }
+        }.resume()
     }
 }
 
